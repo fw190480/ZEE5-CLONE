8c2bee16f4f672ffec0836575d47f7077b996c39
rahul kumar <rkrahul8181@gmail.com>
2023-03-08 08:46:46 +0530

    fixing footer layout for smaller screen

--- frontend/src/Utils/url.links.js
+++ frontend/src/Utils/url.links.js
@@ -1,2 +1,6 @@
-export const development_url = `http://localhost:8080`
-export const production_url = ``
+// for local machine
+// export const development_url = `http://localhost:8080`
+
+// deployed url link => Render
+export const development_url = `https://vidfy.onrender.com`
+// export const production_url = ``
